aa2703c11bb099317c70b08441ed8eee95b1e4d6
sandeep <spatlemail@gmail.com>
2019-01-21 19:03:55 +0100

    Task 3: Add HTTParty to call quandl stock api

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 ruby '2.5.1'
 
 gem 'rspec'
+gem 'httparty'
---
+++ lib/quandl_api/client.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+require 'json'
+
+module QuandlApi
+	class Client
+		include HTTParty
+
+		base_uri 'https://www.quandl.com/api/v3/datasets/EOD'.freeze
+
+		def initialize(options)
+			@stock_symbol = options[:stock_symbol]
+			@options = { query: { api_key: options[:api_key], start_date: options[:start_date], end_date: options[:end_date]} }
+		end
+
+		def get_stock
+			self.class.get(@stock_symbol, @options)
+		rescue HTTParty::Error => e
+			abort e.message
+		end
+	end
+end
