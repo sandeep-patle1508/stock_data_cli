ac992baecd32a66b326456d9939999733c831f0e
sandeep <spatlemail@gmail.com>
2019-01-21 19:07:14 +0100

    Refactor:  Remove unused code

--- lib/stock_data_cli.rb
+++ lib/stock_data_cli.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 Dir.glob('./lib/stock_data_cli/**/*.rb').each do |file|
   require file
 end
-=======
-module StockDataCli
-  # Your code goes here...
-end
->>>>>>> f515276718b7d9a1084b7b10bcac681b1d5c01ac
