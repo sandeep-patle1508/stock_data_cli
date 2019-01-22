ce816b772da07ad70ccdf2a1a10562558c0f9599
sandeep <spatlemail@gmail.com>
2019-01-22 07:47:45 +0100

    Add test cases for argv parser

---
+++ spec/lib/cli_argv_parser_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require './lib/stock_data_cli/cli_argv_parser'
+
+describe StockDataCli::CliArgvParser do
+
+  describe "#parse" do
+    subject { StockDataCli::CliArgvParser.new(argv).parse }
+
+    context "case of valid argv" do
+      let(:argv) { ['API_KEY=test_api_key', 'AAPL', 'jan', '01', '2018', '-', 'jan', '05', '2018'] }
+
+      it "should not raise error" do
+        expect { subject }.not_to raise_error
+      end
+
+      it "should return hash of inputs" do
+        is_expected.to include(:api_key, :stock_symbol, :start_date, :end_date)
+      end
+    end
+
+    context "case of invalid argv" do
+      let(:argv) { ['API_KEY=test_api_key', 'AAPL', 'jan', '01'] }
+
+      it "should raise error" do
+        expect { subject }.to raise_error 'Wrong number of inputs'
+      end
+    end
+
+  end
+end
